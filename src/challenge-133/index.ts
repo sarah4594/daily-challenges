cae324970a32ede15d2764866790582d728d4525
sarah4594 <sarah4594@gmail.com>
2020-01-21 23:14:21 -0600

    🤷‍♀️Possibly fixed Challenge 133
    Added another condition

--- src/challenge-133/index.ts
+++ src/challenge-133/index.ts
@@ -7,9 +7,11 @@ b = b x b     2 = 2 x 2
 n, m = n x n  1, 2 = 1 x 2
 
 so a + b has to be smalle ror equal to m or n (whichever is bigger)
+AND m or n (whichever is smaller) has to be bigger than the biggest size
 
 */
 
 export const fit_in = (a: number, b: number, m: number, n: number): boolean => {
-  return a + b <= Math.max(m, n)
+  if (a + b <= Math.max(m, n)) return Math.min(m, n) >= Math.max(a, b)
+  else return false
 }
