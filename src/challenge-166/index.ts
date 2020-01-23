e5bc62b3eb436098cb2e80ab376bcbed136a673c
sarah4594 <sarah4594@gmail.com>
2020-01-22 19:58:59 -0600

    ⚡️Improve performance of Challenge 166

--- src/challenge-166/index.ts
+++ src/challenge-166/index.ts
@@ -7,26 +7,25 @@ figure out if dog is in between
 */
 
 export const catAndMouse = (j: number, path: string[]) => {
-  let cIndex = 0
-  let mIndex = 0
+  const cIndex = path.indexOf('C')
+  const mIndex = path.indexOf('m')
 
-  for (let i = 0; i <= path.length; i++) {
-    if (path[i] === 'C') {
-      cIndex = i
-    }
-    if (path[i] === 'm') {
-      mIndex = i
-    } else continue
-  }
+  // indexOf (arrays and strings)
+  // for (let i = 0; i < path.length; i++) {
+  //   if (path[i] === 'C') {
+  //     cIndex = i
+  //   }
+  //   if (path[i] === 'm') {
+  //     mIndex = i
+  //   }
+  // }
 
   const cAndMPath = path.slice(
     Math.min(cIndex, mIndex) + 1,
     Math.max(cIndex, mIndex),
   )
 
-  if (cAndMPath.length <= j && cAndMPath.includes('D') === false)
-    return 'CAUGHT!'
-  else if (cAndMPath.length <= j && cAndMPath.includes('D') === true)
-    return 'PROTECTED!'
-  return 'ESCAPED!'
+  const jumpInRange = cAndMPath.length <= j
+  const hasDog = cAndMPath.includes('D')
+  return jumpInRange ? (hasDog ? 'PROTECTED!' : 'CAUGHT!') : 'ESCAPED!'
 }
