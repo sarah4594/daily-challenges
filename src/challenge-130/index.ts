efbaf5f045f81f5002ad5be79a026669acced814
sarah4594 <sarah4594@gmail.com>
2020-01-22 19:57:55 -0600

    ⚡️Improve performance of Challenge 130

--- src/challenge-130/index.ts
+++ src/challenge-130/index.ts
@@ -9,13 +9,10 @@ so if % != 0, no solution exists
 */
 
 export const solve = (sum: number, gcd: number) => {
-  for (let a = 2; a <= sum - 2; a++) {
-    for (let b = 2; b <= sum - 2; b++) {
-      if (a + b === sum) {
-        if (a % gcd === 0 && b % gcd === 0) {
-          return [a, b]
-        } else continue
-      } else continue
+  for (let a = gcd; a <= sum; a += gcd) {
+    const b = sum - a
+    if (b % gcd === 0) {
+      return [a, b]
     }
   }
   return -1
