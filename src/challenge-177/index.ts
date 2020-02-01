7b290edcca23e681473f70eb9dff39f0c233e4d8
sarah4594 <sarah4594@gmail.com>
2020-02-01 11:22:13 -0600

    🔨Use string comparison instead of implicit type conversion

--- src/challenge-177/index.ts
+++ src/challenge-177/index.ts
@@ -2,7 +2,8 @@ export const superSize = (n: number): number => {
   const s = n
     .toString()
     .split('')
-    .sort((a: any, b: any) => b - a)
+    // sort in descending order
+    .sort((a: string, b: string) => (b < a ? -1 : 1))
     .join('')
   return parseInt(s)
 }
