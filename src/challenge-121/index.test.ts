33642accf503f4cb59a87cbb590bc5cd2c0178cf
sarah4594 <sarah4594@gmail.com>
2020-02-13 23:07:01 -0600

    ⚡️Improve Challenge 121

--- src/challenge-121/index.test.ts
+++ src/challenge-121/index.test.ts
@@ -1,14 +1,35 @@
 import { whoHasMostMoney, Student } from '.'
 
-const students: Student[] = [
+const students1: Student[] = [
   new Student('Sarah', 1, 2, 3),
-  new Student('Logan', 3, 2, 1),
+  new Student('Raif', 1, 2, 3),
+  new Student('Yzma', 1, 2, 3),
 ]
+const students2: Student[] = [
+  new Student('Logan', 3, 2, 1),
+  new Student('Sarah', 1, 2, 3),
+  new Student('Raif', 4, 5, 6),
+]
+const students3: Student[] = [
+  new Student('Logan', 4, 5, 6),
+  new Student('Sarah', 1, 2, 3),
+  new Student('Raif', 3, 2, 1),
+]
+const students4: Student[] = [
+  new Student('Logan', 3, 2, 1),
+  new Student('Sarah', 4, 5, 6),
+  new Student('Raif', 1, 2, 3),
+]
+const students5: Student[] = [new Student('Sarah', 1, 2, 3)]
 
 // const who = whoHasMostMoney(students)
 
 describe('whoHasMostMoney', () => {
   it('should add up money of each student and return who has most', () => {
-    expect(whoHasMostMoney(students)).toBe('Sarah: $85')
+    expect(whoHasMostMoney(students1)).toBe('All')
+    expect(whoHasMostMoney(students2)).toBe('Raif: $190')
+    expect(whoHasMostMoney(students3)).toBe('Logan: $190')
+    expect(whoHasMostMoney(students4)).toBe('Sarah: $190')
+    expect(whoHasMostMoney(students5)).toBe('Sarah: $85')
   })
 })
--- src/challenge-121/index.ts
+++ src/challenge-121/index.ts
@@ -3,26 +3,30 @@ export class Student {
   fives: number
   tens: number
   twenties: number
+  total: number
   constructor(name: string, fives: number, tens: number, twenties: number) {
     this.name = name
     this.fives = fives
     this.tens = tens
     this.twenties = twenties
-  }
-  totalAmount() {
-    return this.fives * 5 + this.tens * 10 + this.twenties * 20
+    this.total = this.fives * 5 + this.tens * 10 + this.twenties * 20
   }
 }
 
-export const whoHasMostMoney = (students: Student[]) => {
-  let mostMoney: any = []
-  let name = ''
-  students.forEach(student => {
-    if (student.totalAmount() === mostMoney) return 'All'
-    if (student.totalAmount() > mostMoney) {
-      mostMoney = student.totalAmount()
-      name = student.name
-    }
-  })
-  return `${name}: $${mostMoney}`
+export const whoHasMostMoney = (students: Student[]): string => {
+  students.sort((a, b) => b.total - a.total)
+  const mostMoney = students[0].total
+
+  const isAll =
+    students.length > 1 &&
+    all(students, (student: Student) => student.total === mostMoney)
+  return isAll ? 'All' : `${students[0].name}: $${mostMoney}`
 }
+
+const all = (array: any[], predicate: any) =>
+  array.filter(predicate).length === array.length
+
+const any = (array: any[], predicate: any) => array.filter(predicate).length > 0
+
+const none = (array: any[], predicate: any) =>
+  array.filter(predicate).length === 0
