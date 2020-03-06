c70956f1699ab63f5a8062c17a04ff39dcaba40c
sarah4594 <sarah4594@gmail.com>
2020-03-06 15:16:21 -0600

    ⚡️Optimize Challenge 194

--- src/challenge-194/index.ts
+++ src/challenge-194/index.ts
@@ -1,7 +1,6 @@
-export const spreadNumber = (n: number): number[] => {
-  const arr: number[] = []
-  for (let i = 1; i <= n; i++) {
-    arr.push(i)
-  }
-  return arr
-}
+export const spreadNumber = (n: number): number[] =>
+  new Array<number>(n).fill(0).map((_, index) => index + 1)
+// for (let i = 1; i <= n; i++) {
+//   arr.push(i)
+// }
+//return arr
