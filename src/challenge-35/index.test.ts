b27b16e81fb3afef940dbc01c55e682532c43421
sarah4594 <sarah4594@gmail.com>
2020-03-12 22:58:54 -0500

    ⚡️Optimize Challenge 35 and fix test for 0

--- src/challenge-35/index.test.ts
+++ src/challenge-35/index.test.ts
@@ -5,6 +5,6 @@ describe('findOutlier', () => {
     expect(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])).toBe(11)
     expect(findOutlier([160, 3, 1719, 19, 11, 13, -21])).toBe(160)
     expect(findOutlier([4, 8, 15, 16, 24, 42])).toBe(15)
-    expect(findOutlier([16, 6, 40, 66, 68, 28])).toBe(0)
+    expect(findOutlier([16, 6, 40, 66, 68, 28])).toBe(undefined)
   })
 })
--- src/challenge-35/index.ts
+++ src/challenge-35/index.ts
@@ -1,12 +1,30 @@
-export const findOutlier = (arr: number[]): number => {
-  const even: number[] = []
-  const odd: number[] = []
-  arr.forEach(n => {
+export const findOutlier = (arr: number[]): number | undefined => {
+  // const even: number[] = []
+  // const odd: number[] = []
+  // arr.forEach(n => {
+  //   if (n % 2 === 0) {
+  //     even.push(n)
+  //   } else odd.push(n)
+  // })
+  // if (even.length === 0 || odd.length === 0) return 0
+  // if (even.length === 1) return even[0]
+  // return odd[0]
+
+  let lastEven = 0
+  let lastOdd = 0
+  let evenCount = 0
+  let oddCount = 0
+  for (let i = 0; i < arr.length; i++) {
+    const n = arr[i]
     if (n % 2 === 0) {
-      even.push(n)
-    } else odd.push(n)
-  })
-  if (even.length === 0 || odd.length === 0) return 0
-  if (even.length === 1) return even[0]
-  return odd[0]
+      lastEven = n
+      evenCount++
+    } else {
+      lastOdd = n
+      oddCount++
+    }
+    if (evenCount > 1 && oddCount === 1) return lastOdd
+    if (oddCount > 1 && evenCount === 1) return lastEven
+  }
+  return undefined
 }
