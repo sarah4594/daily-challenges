f89d17f95b76dc11ade9731b57f3b1313f062d15
sarah4594 <sarah4594@gmail.com>
2020-03-16 20:28:05 -0500

    ⚡️Optimize Challenge 38

--- src/challenge-38/index.ts
+++ src/challenge-38/index.ts
@@ -1,12 +1,3 @@
-const middleNames = (name: string[]): string[] => {
-  for (let i = 0; i < name.length; i++) {
-    if (i === 0 || i === name.length - 1) {
-      name.splice(i, 1)
-    }
-  }
-  return name
-}
-
 export const middleInitial = (name: string): string | undefined => {
   // First and Last stay the same
   const regex = name.match(/\w+/g)
@@ -16,9 +7,8 @@ export const middleInitial = (name: string): string | undefined => {
     const first = regex[0]
     const last = regex[regex.length - 1]
     let initials = ''
-    const middles = middleNames(regex)
-    for (let i = 0; i < middles.length; i++) {
-      initials += `${middles[i][0]}. `
+    for (let i = 1; i <= regex.length - 2; i++) {
+      initials += `${regex[i][0]}. `
     }
     return `${first} ${initials}${last}`
   }
