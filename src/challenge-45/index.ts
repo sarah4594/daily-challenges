40084628bb52056f0628b56aec045f1c43a2388c
sarah4594 <sarah4594@gmail.com>
2020-03-21 16:11:38 -0500

    ⚡️Optimize challenge 45

--- src/challenge-45/index.ts
+++ src/challenge-45/index.ts
@@ -1,17 +1,11 @@
 export const change = (n: number): object => {
-  const register = {
-    '25': 0,
-    '10': 0,
-    '5': 0,
-    '1': 0,
-  }
-
-  register[25] = Math.floor(n / 25)
-  const a = n % 25
-  register[10] = Math.floor(a / 10)
-  const b = a % 10
-  register[5] = Math.floor(b / 5)
-  register[1] = b % 5
-
+  const register: any = {}
+  const coins = ['25', '10', '5', '1']
+  let remaining = n
+  coins.forEach(key => {
+    const coin = Number(key)
+    register[key] = Math.floor(remaining / coin)
+    remaining %= coin
+  })
   return register
 }
