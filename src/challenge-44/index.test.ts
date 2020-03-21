bdf341fafd1ae8221e047ec94e19c1ad22c83ecc
sarah4594 <sarah4594@gmail.com>
2020-03-21 16:11:08 -0500

    ⚡️Add support for skipping spaces

--- src/challenge-44/index.test.ts
+++ src/challenge-44/index.test.ts
@@ -4,5 +4,17 @@ describe('wave', () => {
   it('should take a string and return a wave', () => {
     expect(wave('hello')).toEqual(['Hello', 'hEllo', 'heLlo', 'helLo', 'hellO'])
     expect(wave('sarah')).toEqual(['Sarah', 'sArah', 'saRah', 'sarAh', 'saraH'])
+    expect(wave('hello sarah')).toEqual([
+      'Hello sarah',
+      'hEllo sarah',
+      'heLlo sarah',
+      'helLo sarah',
+      'hellO sarah',
+      'hello Sarah',
+      'hello sArah',
+      'hello saRah',
+      'hello sarAh',
+      'hello saraH',
+    ])
   })
 })
--- src/challenge-44/index.ts
+++ src/challenge-44/index.ts
@@ -1,6 +1,7 @@
 export const wave = (s: string): string[] => {
   const arr: string[] = []
   for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') continue
     arr.push(`${s.slice(0, i)}${s[i].toUpperCase()}${s.slice(i + 1, s.length)}`)
   }
   return arr
